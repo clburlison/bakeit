bfc43ca5d1cc3e88a3364e59132d8cd3f858fbd5
Clayton Burlison <git@clburlison.com>
2017-12-20 15:11:51 -0600

    refactor: Prep for platform specific code
    Setup will be our main processing function to get a machine into our
    prep'ed state. This fixes cross platform building since groob/mackit
    will not function on none darwin machines.

---
+++ client/setup_darwin.go
@@ -0,0 +1,92 @@
+package client
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"regexp"
+
+	"github.com/groob/mackit/dmgutils"
+	"github.com/groob/mackit/install/pkg"
+)
+
+func Setup() {
+	user, _ := user.Current()
+	if user.Uid != "0" {
+		fmt.Println("Please run as root!")
+		os.Exit(1)
+	}
+	file, err := Download(".", "https://packages.chef.io/files/stable/chef/13.6.4/mac_os_x/10.13/chef-13.6.4-1.dmg")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error downloading file: %s\n", err)
+		os.Exit(1)
+	}
+
+	pkgPath := ""
+	dmgMountPoint := ""
+
+	// Check for file type
+	ext, mime := Match(file)
+	fmt.Printf("File type: %s. MIME: %s\n", ext, mime)
+
+	// Handle dmgs, pkgs, and unsupported files
+	if ext == "zlib" {
+		mountpoints, err := dmgutils.MountDMG(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Mount failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Mountpoints: %v \n", mountpoints)
+		if len(mountpoints) > 1 {
+			fmt.Fprintf(os.Stderr, "More than one mount path returned\n")
+			os.Exit(1)
+		}
+		dmgMountPoint = mountpoints[0]
+		packages := checkExt(".pkg", dmgMountPoint)
+		fmt.Printf("Packages: %v \n", packages)
+		if len(packages) > 1 {
+			fmt.Fprintf(os.Stderr, "This dmg has more than one package\n")
+			os.Exit(1)
+		} else if packages == nil {
+			fmt.Fprintf(os.Stderr, "Unable to locate packages in this dmg!\n")
+			os.Exit(1)
+		}
+		pkgPath = filepath.Join(dmgMountPoint, packages[0])
+		fmt.Printf("Package Path: %s\n", pkgPath)
+	} else if ext == "xar" {
+		pkgPath = file
+	} else {
+		fmt.Fprintf(os.Stderr, "Unsupported file type was downloaded!\n")
+		os.Exit(1)
+	}
+
+	restart, err := pkg.Install(pkgPath)
+	fmt.Printf("Restart value is: %v \n", restart)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Install failed: %v\n", err)
+	}
+
+	// Unmount dmg files
+	if ext == "zlib" {
+		out, err := dmgutils.UnmountDMG(dmgMountPoint)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+		}
+		fmt.Printf("Unmount result: %t\n", out)
+	}
+}
+
+func checkExt(ext string, path string) []string {
+	var files []string
+	filepath.Walk(path, func(path string, f os.FileInfo, _ error) error {
+		if !f.IsDir() {
+			r, err := regexp.MatchString(ext, f.Name())
+			if err == nil && r {
+				files = append(files, f.Name())
+			}
+		}
+		return nil
+	})
+	return files
+}
---
+++ client/setup_linux.go
@@ -0,0 +1,16 @@
+package client
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func Setup() {
+	user, _ := user.Current()
+	fmt.Printf("User info: %s, %s\n", user.Uid, user.Gid)
+	fmt.Printf("User info: %s, %s\n", user.Username, user.Name)
+	fmt.Printf("User info: %s\n", user.HomeDir)
+	fmt.Printf("Linux is not supported yet!\n")
+	os.Exit(1)
+}
