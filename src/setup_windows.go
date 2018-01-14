d53580da9ad7152ff30e73cff8040d5966c6367f
Clayton Burlison <git@clburlison.com>
2018-01-13 22:39:55 -0600

    feat: Support for windows install
    Windows support for: #7
    Windows support for: #9
    Fix: #23

--- src/setup_windows.go
+++ src/setup_windows.go
@@ -2,24 +2,45 @@ package client
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/user"
 )
 
 func Setup() {
 	// Get the current node serial number
-	serial := GetSerialNumber()
-	fmt.Printf("Current serial number is: %s\n", serial)
+	// TODO: Limit this to window 7+. Will need to verify server lineup as well?
+	clientConfig, err := Config()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create client config:\n%s\n", err)
+		os.Exit(1)
+	}
 
-	user, _ := user.Current()
-	fmt.Printf("User info: %s, %s\n", user.Username)
-	fmt.Printf("User info: %s\n", user.Name)
-	fmt.Printf("User info: %s\n", user.HomeDir)
+	// Write chef files
+	err = ChefFiles(clientConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing chef files: %s\n", err)
+		os.Exit(1)
+	}
 
 	// Install chef if required
-	_, err := InstallChef()
+	_, err = InstallChef()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Bootstrap failed: %s\n", err)
+		os.Exit(1)
+	}
+
+	// TODO: This is very opinionated. Should it be removed or
+	// controlled with a config option?
+	// Set the firstboot tag to ensure the firstboot runlist is used.
+	err = ioutil.WriteFile("C:\\chef\\firstboot", []byte(""), 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create firstboot file:%s\n", err)
+	}
+
+	// Run chef in a loop
+	_, err = RunChef()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Chef failed to complete: %s\n", err)
 		os.Exit(1)
 	}
 }
